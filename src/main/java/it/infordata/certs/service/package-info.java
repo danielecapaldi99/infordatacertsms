56d7a19bb307b90958a1f733d6155d245f0f6b79
danielecapaldi99 <danielecapaldi@outlook.com>
2021-09-28 17:58:25 +0200

    Clean branch for another test

--- src/main/java/it/infordata/certs/service/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Service layer beans.
- */
-package it.infordata.certs.service;
--- ui/widgets/certs/tableWidget/src/state/filter.types.js
+++
@@ -1,4 +0,0 @@
-export const ADD_FILTER = 'certs-filter/addFilter';
-export const UPDATE_FILTER = 'certs-filter/updateFilter';
-export const DELETE_FILTER = 'certs-filter/deleteFilter';
-export const CLEAR_FILTERS = 'certs-filter/clearFilters';
